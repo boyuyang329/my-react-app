c22c5b9cf4f4fbc619100b8853dce05ccf82aa56
boyuyang329 <boyu.y@wustl.edu>
2024-11-13 17:26:12 -0600

    handclick function change

--- src/App.jsx
+++ src/App.jsx
@@ -13,18 +13,9 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null));
 
   function handleClick(i) {
-    if (squares[i]) {
+    if (squares[i] || calculateWinner(squares)) {
       return;
     }
-    const nextSquares = squares.slice();
-    if (xIsNext) {
-      nextSquares[i] = 'X';
-    } else {
-      nextSquares[i] = 'O';
-    }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
-  }
 
   return (
     <>
